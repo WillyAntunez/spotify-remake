f58f044c75123f90f24cfbc5b2e1b6e20b88a3cf
Willy Antunez <willyantunezg@gmail.com>
2023-03-04 10:54:17 -0600

    [feat] PlayerNavbar: Estilos scss basicos finalizados

--- src/player/components/PlayerNavbar.tsx
+++ src/player/components/PlayerNavbar.tsx
@@ -14,22 +14,29 @@ import {
 import { useLocation } from 'react-router-dom';
 
 import userPhoto from '../../assets/img/user-image.jpeg';
+import { useMemo } from 'react';
 
 export const PlayerNavbar = () => {
 
     const location = useLocation();
 
     const userName:string = 'Willy Antunez';
+    const currentColor = [72, 32, 176];
+
+    const [ currentRed, currentGreen, currentBlue ] = currentColor; 
+    const navOpacity = 0; // 0 to 100
+
+    const navBackgroundRgba = useMemo<string>(() => `rgba(${currentRed}, ${currentGreen}, ${currentBlue}, ${navOpacity}%)`, [])
 
     return (
-        <header className='playernav'>
+        <header className='playernav' style={ { backgroundColor: navBackgroundRgba } }>
 
             <div className='playernav__right'>
                 <div className="playernav__arrows">
-                    <button className='playernav__navbtn'>
+                    <button className='playernav__navbtn playernav__navbtn--active'>
                         <PrevPageSvg />
                     </button> 
-                    <button className='playernav__navbtn'>
+                    <button className='playernav__navbtn '>
                         <NextPageSvg />
                     </button>
                     <div className='playernav__context'>
@@ -52,11 +59,11 @@ export const PlayerNavbar = () => {
                 <div className="usermenu">
                     <button className="usermenu__button">
                         <div className="usermenu__photo">
-                            <img src={ userPhoto } alt={userName} />
-                            <span className='usermenu__name'>{ userName }</span>
-                            <span className='usermenu__icon'>
-                                <TriangleBottomSvg />
-                            </span>
+                            <img src={ userPhoto } alt={userName}  />
+                        </div>
+                        <span className='usermenu__name'>{ userName }</span>
+                        <div className='usermenu__icon'>
+                            <TriangleBottomSvg />
                         </div>
                     </button>
                 </div>
