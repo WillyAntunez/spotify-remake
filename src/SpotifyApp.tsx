5b8850a55b2e3479d6d2cf5dad6f6aa45bc37f4e
Willy Antunez <willyantunezg@gmail.com>
2023-03-15 17:45:13 -0600

    [feat] Provider added to the SpotifyApp.tsx

--- src/SpotifyApp.tsx
+++ src/SpotifyApp.tsx
@@ -1,12 +1,16 @@
+import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import { AppRouter } from './router/AppRouter';
 
 import './SpotifyApp.scss';
+import { store } from './store';
 
 export const SpotifyApp = () => {
   return (
-    <BrowserRouter>
-      <AppRouter />
-    </BrowserRouter>
+    <Provider store={ store }>
+      <BrowserRouter>
+        <AppRouter />
+      </BrowserRouter>
+    </Provider>
   )
 }
