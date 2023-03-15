1312bc1728ae9986c8fd2756204ad2e3fa345944
Willy Antunez <willyantunezg@gmail.com>
2023-03-15 17:47:44 -0600

    [Feat] Reactive auth status checking and error handling added to the router

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,19 +1,35 @@
 import { useEffect } from 'react';
-import { Route, Routes } from 'react-router-dom';
-
-import { getToken } from '../api';
+import { Navigate, Route, Routes } from 'react-router-dom';
+import { AuthChecking, AuthError } from '../authorization/pages';
+import { useAuthStore } from '../hooks/useAuthStore';
 
 import { PlayerRoutes } from '../player';
 
-const token = await getToken();
-console.log(token);
 
 export const AppRouter = () => {
-    
+
+    const { checkAuthToken, status } = useAuthStore();
+
+    useEffect(() => {
+        checkAuthToken();
+    }, []);
+
+    if(status === 'CHECKING') {
+        return (
+            <Routes>
+                <Route path='/' element={ <AuthChecking /> } />
+                <Route path='/*' element={ <Navigate to={'/'} /> } />
+            </Routes>
+        )
+    };
 
     return (
         <Routes>
-            <Route path='/*' element={ <PlayerRoutes /> } />
+            {
+                status !== 'ERROR' 
+                ? ( <Route path='/*' element={ <PlayerRoutes /> } /> )
+                : ( <Route path='/*' element={ <AuthError /> } />  )
+            }     
         </Routes>
     )
 };
