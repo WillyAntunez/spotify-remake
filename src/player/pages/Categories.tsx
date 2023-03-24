6570b895901bde52b67253aa0c2aa79542cf7fc1
Willy Antunez <willyantunezg@gmail.com>
2023-03-23 19:45:24 -0600

    [Fix] Now each page in the player view implements the footer only if they want and where they want

--- src/player/pages/Categories.tsx
+++ src/player/pages/Categories.tsx
@@ -5,12 +5,13 @@ import { Category } from '../../utils/types'
 
 import './Categories.scss';
 import { CategoryCard } from '../components/CategoryCard';
+import { PlayerViewFooter } from '../components';
 
 export const Categories = () => {
 
   const [categories, setCategories] = useState<Category[]>([]);
 
-  const setInitialCategories = async ():Promise<void> => {
+  const setInitialCategories = async (): Promise<void> => {
     try {
       const initialCategories = await getCategories(0, 50)
       setCategories(initialCategories.items);
@@ -25,19 +26,23 @@ export const Categories = () => {
   }, [])
 
   return (
-    <div className="categories">
-      <h2 className="categories__title">Explorar todo</h2>
-      <div className="categories__grid">
-        
-        {
-          categories.length > 0
-          ? categories.map( category => 
-              <CategoryCard category={category} key={category.id} />              
-            ) 
-          : null
-        }
+    <>
+      <div className="categories">
+        <h2 className="categories__title">Explorar todo</h2>
+        <div className="categories__grid">
 
+          {
+            categories.length > 0
+              ? categories.map(category =>
+                <CategoryCard category={category} key={category.id} />
+              )
+              : null
+          }
+
+        </div>
       </div>
-    </div>
+      <PlayerViewFooter />
+    </>
+
   )
 }
